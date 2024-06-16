719cb8f6eeaa7e0e393b327d3386e5cff2048545
bjd169 <bjorgedamas0904@gmail.com>
2024-06-16 13:34:50 +0200

    Update DecodificadorUsr.java

--- src/main/java/com/mycompany/grupo9/DecodificadorUsr.java
+++ src/main/java/com/mycompany/grupo9/DecodificadorUsr.java
@@ -9,11 +9,16 @@ import com.mycompany.grupo9.paquetes.PaqueteUsr;
 
 
 /**
- *
- * @author javier
+ * Clase que implementa el decodificador de usuarios
+ * @author Javier Zataraín
+ * @author Blanca Jorge
  */
 public class DecodificadorUsr implements Decodificador {
-    
+    /**
+     * Decodifica una cadena de texto en un objeto de la clase
+     * @param linea Línea de texto que contiene los datos del usuario
+     * @return paquete Objeto que contiene los datos decodificados
+     */
     @Override
     public PaqueteUsr decodificar(String linea){
         PaqueteUsr paquete = new PaqueteUsr();
