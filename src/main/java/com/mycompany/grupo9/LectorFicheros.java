ce614b33579fc6df05ebca651f55ed8acec16608
bjd169 <bjorgedamas0904@gmail.com>
2024-06-16 13:35:41 +0200

    Update LectorFicheros.java

--- src/main/java/com/mycompany/grupo9/LectorFicheros.java
+++ src/main/java/com/mycompany/grupo9/LectorFicheros.java
@@ -14,11 +14,17 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 /**
- *
- * @author javier
+ * Clase encargada de la lectura y el guardado de información en los ficheros
+ * @author Javier Zataraín
+ * @author Blanca Jorge
  */
 public class LectorFicheros {
-    
+    /**
+     * Lee el archivo de los usuarios y devuleve un hashmap con los nombres y las
+     * contraseñas de estos
+     * @param linea Nombre del archivo donde se incluyen estos datos
+     * @return listado HashMap con el nombre de usuario y las contraseñas
+     */
     public static HashMap<String,String> lecturaFichero(String linea){
        HashMap<String,String> listado = new HashMap();
        try (BufferedReader br = new BufferedReader(new FileReader(linea))) {
@@ -32,14 +38,22 @@ public class LectorFicheros {
        
        return listado;
     }
-    
+    /**
+     * Lee el archivo que incluye las partidas
+     * @param linea Nombre del archivo que contine las partidas
+     * @return listaPartidas Listado de las partidas
+     */
     public static ArrayList<PaquetePartida> lecturaPartidas(String linea){
         ArrayList<PaquetePartida> listaPartidas = new ArrayList();
 
         
         return listaPartidas;
     }
-    
+    /**
+     * Lee el archivo de las puntuaciones
+     * @param linea Nombre del archivo que contiene las puntuaciones
+     * @return ranking Las puntuaciones
+     */
     public static ArrayList<String> lecturaPuntuaciones(String linea){
         ArrayList<String> ranking = new ArrayList();
         try (BufferedReader br = new BufferedReader(new FileReader(linea))) {
@@ -52,7 +66,11 @@ public class LectorFicheros {
         }
         return ranking;
     }
-    
+    /**
+     * Guarda las puntuaciones actualizadas en el archivo correspondiente
+     * @param linea Nombre del archivo de la puntuaciones
+     * @param array Lista de cadenas que representan las puntuaciones
+     */
     public static void guardaPuntuaciones(String linea, ArrayList<String> array){
         try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(linea))) {
             for (String line : array) {
@@ -64,7 +82,11 @@ public class LectorFicheros {
                 System.out.println("\nError en el guardado de Puntuaciones...");
         }
     }
-    
+    /**
+     * Guarda los usuarios y las contraseñas
+     * @param linea Nombre del archivo de los usuarios
+     * @param usuarios HashMap con los nombres y contraseñas de los usuarios
+     */
     public static void guardaUsr(String linea, HashMap<String,String> usuarios){
         try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(linea))) {
             for (String usr : usuarios.keySet()) {
@@ -77,7 +99,11 @@ public class LectorFicheros {
                 System.out.println("\nError en el guardado de Usuarios...");
         }
     }
-    
+    /**
+     * Guarda en el archivo las plantillas
+     * @param linea Nombre del archivo donde se guardan las partidas
+     * @param array Lista que representa las partidas
+     */
     public static void guardaPartidas(String linea, ArrayList<PaquetePartida> array){
         try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(linea))) {
             for (PaquetePartida partida : array) {
